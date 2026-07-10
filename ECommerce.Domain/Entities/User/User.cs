4dc59bccbaa9759133cd4830a364f5f8613c74de
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2026-07-10 11:36:04 +0100

    add unit test projectc

--- ECommerce.Domain/Entities/User/User.cs
+++ ECommerce.Domain/Entities/User/User.cs
@@ -5,15 +5,15 @@ namespace ECommerce.Domain.Entities.User;
 
 public class User : AuditableEntity<Guid>
 {
-    public required string FirstName { get; set; }
+    public required string FirstName { get; set; } = string.Empty;
 
-    public required string LastName { get; set; }
+    public required string LastName { get; set; } = string.Empty;
 
-    public required string Email { get; set; }
+    public required string Email { get; set; } = string.Empty;
 
-    public required string PasswordHash { get; set; }
+    public required string PasswordHash { get; set; } = string.Empty;
 
-    public required string Phone { get; set; }
+    public required string Phone { get; set; } = string.Empty;
 
     public bool IsEmailVerified { get; set; }
 
