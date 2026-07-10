957bb4a755e617f1a20901c65c04cb628168cdd4
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2026-07-10 11:12:57 +0100

    Add architecture tests

---
+++ ECommerce.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ReflectionAssembly = System.Reflection.Assembly;
+
+namespace ECommerce.ArchitectureTests;
+
+public abstract class BaseTest
+{ 
+    protected static readonly ReflectionAssembly DomainAssembly = typeof(Domain.AssemblyMarker).Assembly;
+    protected static readonly ReflectionAssembly ApplicationAssembly = typeof(Application.AssemblyMarker).Assembly;
+    protected static readonly ReflectionAssembly InfrastructureAssembly = typeof(Infrastructure.AssemblyMarker).Assembly;
+    protected static readonly ReflectionAssembly ApiAssembly = typeof(Api.AssemblyMarker).Assembly;
+
+    protected static readonly Architecture Architecture = new ArchLoader()
+        .LoadAssemblies(
+            DomainAssembly,
+            ApplicationAssembly,
+            InfrastructureAssembly,
+            ApiAssembly)
+        .Build();
+}
---
+++ ECommerce.ArchitectureTests/VisibilityTests.cs
@@ -0,0 +1,24 @@
+﻿using ArchUnitNET.xUnit;
+using ECommerce.Application.Abstractions.Messaging;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ECommerce.ArchitectureTests;
+
+public class VisibilityTests : BaseTest
+{
+    [Fact]
+    public void CommandHandlers_ShouldBeInternal() => Classes()
+        .That()
+        .ImplementInterface(typeof(ICommandHandler<,>))
+        .Should()
+        .BeInternal()
+        .Check(Architecture);
+
+    [Fact]
+    public void QueryHandlers_ShouldBeInternal() => Classes()
+            .That()
+            .ImplementInterface(typeof(IQueryHandler<,>))
+            .Should()
+            .BeInternal()
+            .Check(Architecture);
+}
