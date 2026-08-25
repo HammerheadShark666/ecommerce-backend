cedde4eb958d1299ea262fdc5ebf578298adb132
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2026-08-25 16:27:50 +0100

    create 2fa status check based on password reset token

---
+++ ECommerce.Application/Features/Security/CheckTwoFactorStatus/ByPasswordResetToken/GetTwoFactorStatusByTokenQueryHandler.cs
@@ -0,0 +1,31 @@
+using ECommerce.Application.Abstractions;
+using ECommerce.Application.Abstractions.Messaging;
+using ECommerce.Application.Common.Errors;
+using ECommerce.Application.Features.Security.CheckTwoFactorStatus.ByPasswordResetToken;
+using ECommerce.Domain.Entities.User;
+using FluentResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Application.Features.Security.CheckTwoFactorStatus;
+ 
+internal class GetTwoFactorStatusByTokenQueryHandler(IECommerceDbContext dbContext) : IQueryHandler<GetTwoFactorStatusByTokenQuery, GetTwoFactorStatusByTokenResponse>
+{ 
+    public async Task<Result<GetTwoFactorStatusByTokenResponse>> Handle(GetTwoFactorStatusByTokenQuery request, CancellationToken cancellationToken)
+    {
+        var token = request.Token.Trim().ToUpperInvariant(); 
+
+        var user = await GetUserAsync(token, cancellationToken);
+        if (user is null)
+        {
+            return Result.Fail<GetTwoFactorStatusByTokenResponse>(new InvalidCredentialsError());
+        }
+
+        return Result.Ok(new GetTwoFactorStatusByTokenResponse(user.IsTwoFactorEnabled)); 
+    }
+
+    private async Task<User?> GetUserAsync(string token, CancellationToken cancellationToken) =>
+                await dbContext.PasswordResetTokens
+                    .Where(x => x.TokenHash == token)
+                    .Select(x => x.User)
+                    .FirstOrDefaultAsync(cancellationToken);           
+}
---
+++ ECommerce.Application/Features/Security/CheckTwoFactorStatus/ByPasswordResetToken/GetTwoFactorStatusByTokenValidator.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Application.Features.Security.CheckTwoFactorStatus.ByPasswordResetToken;
+using FluentValidation;
+
+namespace ECommerce.Application.Features.Security.CheckTwoFactorStatus;
+ 
+public class GetTwoFactorStatusByTokenValidator : AbstractValidator<GetTwoFactorStatusByTokenQuery>
+{
+    public GetTwoFactorStatusByTokenValidator() => RuleFor(x => x.Token)
+            .NotEmpty()
+            .MaximumLength(100)
+            .Must(BeValidBase64)
+            .WithMessage("Invalid password reset token.");
+
+    private static bool BeValidBase64(string token)
+    {
+        try
+        {
+            var bytes = Convert.FromBase64String(token);
+            return bytes.Length == 32;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+    }
+}
