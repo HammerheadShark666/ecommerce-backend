c4f5c2e92b5b6e66a0cabb82232cf49831275edb
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2026-08-29 16:57:18 +0100

    Change AccessToken in response to JwtToken

--- ECommerce.Application/Features/Security/Authentication/TwoFactorLogin/TwoFactorLoginCommand.cs
+++ ECommerce.Application/Features/Security/Authentication/TwoFactorLogin/TwoFactorLoginCommand.cs
@@ -4,6 +4,6 @@ namespace ECommerce.Application.Features.Security.Authentication.TwoFactorLogin;
  
 public record TwoFactorLoginCommand(string Email, string PendingToken, string Code, Guid PendingTokenId) : ICommand<TwoFactorLoginResponse>;
 
-public record TwoFactorLoginResponse(string? Token, string RefreshToken);
+public record TwoFactorLoginResponse(string? JwtToken, string RefreshToken);
 
-public record LoginResponse(string? Token);
+//public record LoginResponse(string? JwtToken);
